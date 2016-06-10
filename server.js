3ae194dc703d1d4bc0ead4b775a82627dca4e41d
nadien <sergiomen4@gmail.com>
2016-06-10 00:58:24 -0500

    adding crawlme for snapshots html

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ var getToken = require('./app/scripts/middleware/getRole');
 var uploadImg = require('./app/scripts/middleware/Upload');
 var sidebar = require('./app/scripts/middleware/Sidebar');
 var phantom = require('node-phantom');
-var seo = require('mean-seo');
+  var connect = require('connect'),
+     crawlme = require('crawlme');
     //..localhost/[name] <-- indica la base de datos a usar en mongdb
     mongoose.connect('mongodb://localhost/faptoriaMujeres');
 
@@ -27,29 +28,29 @@ var seo = require('mean-seo');
     app.use(express.static(__dirname + '/app'));
     app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-   app.use(seo({
-    cacheClient: 'disk', // Can be 'disk' or 'redis'
-  //  redisURL: 'redis://:password@hostname:port', // If using redis, optionally specify server credentials
-    cacheDuration: 2 * 60 * 60 * 24 * 1000, // In milliseconds for disk cache
-    }));
+    
+
+    var appCrawl = connect().use(crawlme()).use(connect(__dirname + '/webroot'));
+
+    http.createServer(appCrawl).listen(3000);
 
 //Iniciando el servidor de imagenes con Multer
     app.get('/uploads/:id' , function(req , res){
         res.sendFile(__dirname+'/uploads/' + req.params.id);
     });
 
-    app.use(login);
-    app.use(getToken);
-    app.use(uploadImg);
-    app.use(sidebar);
+app.use(login);
+app.use(getToken);
+app.use(uploadImg);
+app.use(sidebar);
 
 
-    app.all('/*', function(req, res, next) {
+ app.all('/*', function(req, res, next) {
       // Just send the index.html for other files to support HTML5Mode
       res.sendFile('/app/index.html', { root: __dirname });
-    });
+});
 
-    app.use(function (request, response, next) {
+app.use(function (request, response, next) {
  
     var pageUrl = request.query["_escaped_fragment_"];
  
